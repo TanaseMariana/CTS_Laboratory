40b0ad915209b7d736a2c4853ba9791a5c919190
TanaseMariana <mary32699@gmail.com>
2021-03-09 15:31:27 +0200

    Final commit

--- Week3/src/ro/ase/csie/cts/g1094/refactor/phase1/Product.java
+++ Week3/src/ro/ase/csie/cts/g1094/refactor/phase1/Product.java
@@ -1,11 +1,21 @@
 package ro.ase.csie.cts.g1094.refactor.phase1;
 
+import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidAgeExceptions;
+import ro.ase.csie.cts.g1094.refactor.exceptions.InvalidPriceException;
+
 public class Product {
 	
 	public static final int MAX_AGE_ACCOUNT =10;
 	public static final float MAX_FIDELITY_DISCOUNT = 0.15f;
-	public float computePriceWithDiscount(ProductType productType, float price, int accountAge)
+	public float computePriceWithDiscount(ProductType productType, float price, int accountAge) throws InvalidPriceException, InvalidAgeExceptions
 	  {
+		if(price<=0) {
+			throw new InvalidPriceException();
+		}
+		if(accountAge<0) {
+			throw new InvalidAgeExceptions();
+		}
+		
 	    float finalPrice = 0;
 	    float fidelityDiscount = (accountAge > MAX_AGE_ACCOUNT) ? MAX_FIDELITY_DISCOUNT : (float)accountAge/100; 
 	    
