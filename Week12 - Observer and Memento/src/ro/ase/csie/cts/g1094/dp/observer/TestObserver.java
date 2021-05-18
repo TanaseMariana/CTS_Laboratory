ab93b9de3053db0935364d3a052daa5c2a32853d
Mariana <mary32699@gmail.com>
2021-05-18 14:46:08 +0300

    Implementing Observer and Memento

---
+++ Week12 - Observer and Memento/src/ro/ase/csie/cts/g1094/dp/observer/TestObserver.java
@@ -0,0 +1,27 @@
+package ro.ase.csie.cts.g1094.dp.observer;
+
+public class TestObserver {
+
+	public static void main(String[] args) {
+		
+		
+		NetworkMonitorModule monitor = new NetworkMonitorModule();
+		
+		monitor.networkStatusChanged(NetworkStatus.UP);
+		
+		NetworkStatusHandlerInterface autosave = new AutoSaveModule();
+		NetworkStatusHandlerInterface notifications= new UINotificationModule();
+		
+		monitor.register(autosave);
+		monitor.register(notifications);
+		
+		monitor.networkStatusChanged(NetworkStatus.DOWN);
+		
+		
+		monitor.unregister(notifications);
+		
+		monitor.networkStatusChanged(NetworkStatus.UP);
+
+	}
+
+}
